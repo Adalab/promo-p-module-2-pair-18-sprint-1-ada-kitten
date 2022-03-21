959db939b74aea595fa02976b21a989532bfebe4
Rosa Codina <rosa.codina90@gmail.com>
2022-03-21 11:14:57 +0100

    change nameCat1 to UpperCase but it is pending to do remove the https from catImage1

--- js/main.js
+++ js/main.js
@@ -2,10 +2,27 @@
 
 document.querySelector('.js-new-form').classList.remove("collapsed");
 
-const jsList = document.querySelector(".js-list");
-const listCard1 = '<li class="card"><article><img class="card_img" src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg" alt="gatito" /> <h3 class="card_title">Anastacio</h3> <h4 class="card_race">British Shorthair</h4> <p class="card_description">Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! </p> </article></li>';
-const listCard2 = ' <li class="card"><img class="card_img" src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" alt="gatito" /> <h3 class="card_title">Fiona</h3> <h4 class="card_race">British Shorthair</h4> <p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! </p> </li>';
+const catName1 = "Anastacio";
+function removeHttps(url) {
+    return url.replace(/^https?:\/\//, '');
+}
+const catImage1 = removeHttps("https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg");
 
-const listCard3 = '<li class="card"> <img class="card_img" src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg" alt="gatito"/> <h3 class="card_title">Cielo</h3> <h4 class="card_race">British Shorthair</h4> <p class="card_description"> Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! </p></li>';
+const catRace = "British Shorthair";
+const catDescription = "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+
+const catName2 = "Fiona";
+const catImage2 = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
+
+const catName3 = "Fiona";
+const catImage3 = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
+
+
+const jsList = document.querySelector(".js-list");
+const listCard1 = `<li class="card"><article><img class="card_img" src=${catImage1} alt="gatito" /> <h3 class="card_title">${catName1.toUpperCase()} </h3> <h4 class="card_race">${catRace}</h4> <p class="card_description">${catDescription} </p> </article></li>`;
+
+const listCard2 = `<li class="card"><img class="card_img" src=${catImage2} alt="gatito" /> <h3 class="card_title">${catName2}</h3> <h4 class=>${catRace}</h4> <p class="card_description">${catDescription} </p> </li>`;
+
+const listCard3 = `<li class="card"> <img class="card_img" src=${catImage3} alt="gatito"/> <h3 class="card_title">${catName3}</h3> <h4 class="card_race">${catRace}</h4> <p class="card_description">${catDescription}</p></li>`;
 
 jsList.innerHTML = listCard1 + listCard2 + listCard3;
