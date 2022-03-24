fbcee4e125a15ec9a331600c631c3260575332c6
Rosa Codina <rosa.codina90@gmail.com>
2022-03-24 11:26:04 +0100

    functions

--- js/main.js
+++ js/main.js
@@ -1,47 +1,49 @@
-"use strict";
-const newForm = document.querySelector(".js-new-form");
+'use strict';
+const newForm = document.querySelector('.js-new-form');
 
-const catName1 = "Anastacio";
-const catImage1 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const catName1 = 'Anastacio';
+const catImage1 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 
-const catRace1 = "British Shorthair";
+const catRace1 = 'British Shorthair';
 const catDescription1 =
-  "Risueño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+  'Risueño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 
-const catName2 = "Fiona";
+const catName2 = 'Fiona';
 const catImage2 =
-  "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
-const catRace2 = "British Shorthair";
+  'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+const catRace2 = 'British Shorthair';
 const catDescription2 =
-  "Risueño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+  'Risueño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 
-const catName3 = "Cielo";
+const catName3 = 'Cielo';
 const catImage3 =
-  "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
-const catRace3 = "British Shorthair";
+  'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
+const catRace3 = 'British Shorthair';
 const catDescription3 =
-  "Risueño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+  'Risueño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 
-const inputSearchDesc = document.querySelector(".js_in_search_desc");
-const inputSearchRace = document.querySelector(".js_in_search_race");
+const inputSearchDesc = document.querySelector('.js_in_search_desc');
+const inputSearchRace = document.querySelector('.js_in_search_race');
 // inputSearchDesc.value = "cariñoso";
 const valueInDesc = inputSearchDesc.value;
 const valueInRace = inputSearchRace.value;
 
 const descrSearchText = inputSearchDesc.value;
-const jsList = document.querySelector(".js-list");
-const card = document.querySelector(".card");
-const plusItem = document.querySelector(".item");
+const jsList = document.querySelector('.js-list');
+const card = document.querySelector('.card');
+const plusItem = document.querySelector('.item');
 const btnAdd = document.querySelector('.js-btn-add');
-const inputDesc = document.querySelector(".js-input-desc");
-const inputPhoto = document.querySelector(".js-input-photo");
-const inputName = document.querySelector(".js-input-name");
-const labelMesageError = document.querySelector(".js-label-error");
+const inputDesc = document.querySelector('.js-input-desc');
+const inputPhoto = document.querySelector('.js-input-photo');
+const inputName = document.querySelector('.js-input-name');
+const labelMesageError = document.querySelector('.js-label-error');
+const inputRace = document.querySelector('.js-inputRace');
 const valueDesc = inputDesc.value;
 const valuePhoto = inputPhoto.value;
 const valueName = inputName.value;
-const searchBtn = document.querySelector(".js-search-btn");
-const labelSearchError = document.querySelector(".js-label-search-error");
+const valueRace = inputRace.value;
+const searchBtn = document.querySelector('.js-search-btn');
+const labelSearchError = document.querySelector('.js-label-search-error');
 if (catDescription1.includes(descrSearchText)) {
   const listCard1 = `<li class="card"><article><img class="card_img" src=${catImage1} alt="gatito" /> <h3 class="card_title">${catName1.toUpperCase()} </h3> <h4 class="card_race">${catRace1}</h4> <p class="card_description">${catDescription1} </p> </article></li>`;
   jsList.innerHTML = jsList.innerHTML + listCard1;
@@ -67,42 +69,74 @@ jsList.innerHTML = listCard1 + listCard2 + listCard3;
 */
 
 if (catDescription1.includes(descrSearchText)) {
-  jsList.classList.remove("collapsed");
+  jsList.classList.remove('collapsed');
 } else {
-  jsList.classList.add("collapsed");
+  jsList.classList.add('collapsed');
 }
 
 if (catDescription1.includes(descrSearchText)) {
-  jsList.classList.remove("collapsed");
+  jsList.classList.remove('collapsed');
 } else {
-  jsList.classList.add("collapsed");
+  jsList.classList.add('collapsed');
 }
 
 if (catDescription1.includes(descrSearchText)) {
-  jsList.classList.remove("collapsed");
+  jsList.classList.remove('collapsed');
 } else {
-  jsList.classList.add("collapsed");
+  jsList.classList.add('collapsed');
 }
 
-
-plusItem.addEventListener("click", () => {
-  /* if (newForm.classList.contains("collapsed")) {
+/*plusItem.addEventListener('click', () => {
+   if (newForm.classList.contains("collapsed")) {
     newForm.classList.remove("collapsed");
   } else {
     newForm.classList.add("collapsed");
-  } */
+  } 
   newForm.classList.toggle('collapsed');
+}); */
+
+btnAdd.addEventListener('click', () => {
+  if (valueDesc === '' && valuePhoto === '' && valueName === '') {
+    labelMesageError.innerHTML = 'Debe rellenar todos los valores.';
+  }
 });
 
-btnAdd.addEventListener("click", ()=>{
-  if(valueDesc === "" && valuePhoto === "" && valueName === "") {
-    labelMesageError.innerHTML = "Debe rellenar todos los valores.";
-  }
-})
-
-searchBtn.addEventListener("click", (event)=> {
+searchBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  if(valueInDesc === "" && valueInRace === "") {
-    labelSearchError.innerHTML = "Debe rellenar todos los valores.";
+  if (valueInDesc === '' && valueInRace === '') {
+    labelSearchError.innerHTML = 'Debe rellenar todos los valores.';
   }
-})
+});
+
+function showNewCatForm() {
+  newForm.classList.remove('collapsed');
+}
+//usarla para quitar collapsed y que aparezca el menú
+
+function hideNewCatForm() {
+  newForm.classList.add('collapsed');
+}
+
+plusItem.addEventListener('click', handleClickNewCatForm);
+
+function handleClickNewCatForm(event) {
+  event.preventDefault();
+  if (newForm.classList.contains('collapsed')) {
+    //el formulario está oculto
+    showNewCatForm();
+  } else {
+    hideNewCatForm();
+  }
+}
+
+function renderKitten(valueDesc, valuePhoto, valueName, valueRace) {
+  const result = valueDesc + valuePhoto + valueName + +valueRace;
+
+  return result;
+}
+
+btnAdd.addEventListener('click', addNewKitten);
+
+function addNewKitten(event) {
+  return;
+}
